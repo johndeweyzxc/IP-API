bc2f85d25e68c867741d2c56facbef2a2b567846
johndeweyzxc <johndewey02003@gmail.com>
2023-02-26 19:30:48 +0800

    Added date as request parameter

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const http = require("http");
 const app = express();
 const port = 3000;
 
-app.get("/:ip", (req, res) => {
+app.get("/:ip/:date", (req, res) => {
+  console.log("Date in request parameter: ", req.params.date);
+  console.log("New request coming from: ", req.params.ip);
+
   http
     .get(`http://ip-api.com/json/${req.params.ip}`, (ipRes) => {
       let data = [];
-      const headerDate =
-        ipRes.headers && ipRes.headers.date
-          ? ipRes.headers.date
-          : "no response date";
-      console.log("Status Code:", ipRes.statusCode);
-      console.log("Date in Response header:", headerDate);
 
       ipRes.on("data", (chunk) => {
         data.push(chunk);
@@ -20,13 +17,11 @@ app.get("/:ip", (req, res) => {
 
       ipRes.on("end", () => {
         const ipInfo = JSON.parse(Buffer.concat(data).toString());
-        console.log(`Request from: ${ipInfo.query}`);
         return res.send({
           countryCode: ipInfo.countryCode,
           ip: ipInfo.query,
           isp: ipInfo.isp,
           region: ipInfo.region,
-          userAgent: ipInfo.userAgent,
         });
       });
     })
@@ -36,5 +31,5 @@ app.get("/:ip", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`IP API Server started listening on port:${port}`);
 });
